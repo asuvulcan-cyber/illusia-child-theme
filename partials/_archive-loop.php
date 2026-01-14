ee4315f91c70cb95fa5b4c51c1d98048e2ba80a2
Asu Sinvera <asuvulcan@gmail.com>
2026-01-14 14:25:58 -0300

    Add files via upload

---
+++ partials/_archive-loop.php
@@ -0,0 +1,102 @@
+<?php
+/**
+ * Partial: Archive Loop
+ *
+ * Renders a list of post type specific cards.
+ *
+ * @package WordPress
+ * @subpackage Fictioneer
+ * @since 3.0
+ * @see category.php
+ * @see tag.php
+ * @see taxonomy-fcn_character.php
+ * @see taxonomy-fcn_content_warning.php
+ * @see taxonomy-fcn_fandom.php
+ * @see taxonomy-fcn_genre.php
+ * @see taxonomy-cover_designer.php
+ * @internal $args['taxonomy']  The current taxonomy type.
+ */
+
+
+// No direct access!
+defined( 'ABSPATH' ) OR exit;
+
+// Setup
+$page = get_query_var( 'paged', 1 ) ?: 1; // Main query
+$order = fictioneer_sanitize_query_var( $_GET['order'] ?? 0, ['desc', 'asc'], 'desc' );
+$orderby = fictioneer_sanitize_query_var( $_GET['orderby'] ?? 0, fictioneer_allowed_orderby(), 'modified' );
+$ago = $_GET['ago'] ?? 0;
+$ago = is_numeric( $ago ) ? absint( $ago ) : sanitize_text_field( $ago );
+
+// Prepare sort-order-filter arguments
+$hook_args = array(
+  'page' => $page,
+  'order' => $order,
+  'orderby' => $orderby,
+  'ago' => $ago,
+  'taxonomy' => $args['taxonomy']
+);
+
+?>
+
+<?php do_action( 'fictioneer_archive_loop_before', $hook_args ); ?>
+
+<?php if ( have_posts() ) : ?>
+
+  <section class="archive__posts container-inline-size">
+    <ul id="archive-list" class="scroll-margin-top card-list _no-mutation-observer">
+      <?php
+        while ( have_posts() ) {
+          the_post();
+
+          // Setup
+          $type = get_post_type();
+          $card_args = array(
+            'cache' => fictioneer_caching_active( 'card_args' ) && ! fictioneer_private_caching_active(),
+            'show_type' => true,
+            'order' => $order,
+            'orderby' => $orderby,
+            'ago' => $ago
+          );
+
+          // Special conditions for chapters...
+          if ( $type == 'fcn_chapter' ) {
+            if (
+              get_post_meta( $post->ID, 'fictioneer_chapter_no_chapter', true ) ||
+              get_post_meta( $post->ID, 'fictioneer_chapter_hidden', true )
+            ) {
+              continue;
+            }
+          }
+
+          // Echo correct card
+          fictioneer_echo_card( $card_args );
+        }
+
+        // Pagination
+        $pag_args = array(
+          'prev_text' => fcntr( 'previous' ),
+          'next_text' => fcntr( 'next' ),
+          'add_fragment' => '#archive-list'
+        );
+
+        if ( $GLOBALS['wp_query']->found_posts > get_option( 'posts_per_page' ) ) {
+          ?><li class="pagination"><?php echo fictioneer_paginate_links( $pag_args ); ?></li><?php
+        }
+      ?>
+    </ul>
+  </section>
+
+<?php else : ?>
+
+  <section class="archive__posts container-inline-size">
+    <ul id="archive-list" class="scroll-margin-top card-list _no-mutation-observer">
+      <li class="no-results">
+        <span><?php _e( 'No matching posts found.', 'fictioneer' ); ?></span>
+      </li>
+    </ul>
+  </section>
+
+<?php endif; ?>
+
+<?php do_action( 'fictioneer_archive_loop_after', $hook_args ); ?>
---
+++ partials/_story-header.php
@@ -0,0 +1,97 @@
+<?php
+/**
+ * Partial: Story Header
+ *
+ * Rendered in the single-fcn_story.php template right after opening
+ * the <article> block.
+ *
+ * @package WordPress
+ * @subpackage Fictioneer
+ * @since 5.0.0
+ * @since 5.20.0 - Added Elementor support.
+ * @see single-fcn_story.php
+ *
+ * @internal $args['story_data']  Story data from fictioneer_get_story_data().
+ * @internal $args['story_id']    Current story and post ID.
+ * @internal $args['context']     Render context. Default null.
+ */
+
+
+// No direct access!
+defined( 'ABSPATH' ) OR exit;
+
+// Elementor?
+if ( function_exists( 'elementor_theme_do_location' ) && elementor_theme_do_location( 'story_header' ) ) {
+  return;
+}
+
+// Setup
+$story = $args['story_data'];
+$story_id = $args['story_id'];
+$post = get_post( $story_id );
+$cover_position = get_theme_mod( 'story_cover_position', 'top-left-overflow' );
+
+$thumbnail_shown = has_post_thumbnail( $story_id ) &&
+  ! get_post_meta( $story_id, 'fictioneer_story_no_thumbnail', true ) &&
+  ! in_array( $cover_position, ['float-left', 'float-right', 'hide'] );
+
+$tax_shown = ! get_option( 'fictioneer_hide_taxonomies_on_pages' ) &&
+  $story['has_taxonomies'] &&
+  ! get_post_meta( $story_id, 'fictioneer_story_no_tags', true );
+
+// Story header classes
+$header_classes = ['story__header'];
+
+if ( ! $tax_shown ) {
+  $header_classes[] = '_no-tax';
+}
+
+if ( ! $thumbnail_shown ) {
+  $header_classes[] = '_no-thumbnail';
+}
+
+if ( $cover_position === 'float-top-left' ) {
+  $header_classes[] = '_float-top-left';
+}
+
+if ( $cover_position === 'float-top-right' ) {
+  $header_classes[] = '_float-top-right';
+}
+
+// Filter classes
+$header_classes = apply_filters( 'fictioneer_filter_story_header_classes', $header_classes, $args );
+
+?>
+
+<header class="<?php echo implode( ' ', $header_classes ); ?>">
+
+  <?php if ( $tax_shown ) : ?>
+    <div class="story__taxonomies tag-group"><?php
+      echo fictioneer_get_taxonomy_pills(
+        array(
+          'fandoms' => $story['fandoms'],
+          'genres' => $story['genres'],
+       // 'characters' => $story['characters']
+        ),
+        'story_header'
+      );
+    ?></div>
+    <div class="story__taxonomies-space"></div>
+  <?php endif; ?>
+
+  <?php if ( $thumbnail_shown ) echo fictioneer_get_story_page_cover( $story ); ?>
+
+  <div class="story__identity"><?php
+    $identity = [];
+    $identity['title'] = '<h1 class="story__identity-title">' . $story['title'] . '</h1>';
+    $identity['meta'] = '<div class="story__identity-meta">' . sprintf(
+        _x( 'by %s', 'Story page: by {Author(s)}', 'fictioneer' ),
+        fictioneer_get_story_author_nodes( $story_id )
+      ) . '</div>';
+    $identity = apply_filters( 'fictioneer_filter_story_identity', $identity, $story_id, $story );
+
+    echo implode( '', $identity );
+  ?></div>
+
+</header>
+
